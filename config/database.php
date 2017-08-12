52495634daac1d0f9e30b52af8680bfaf18af07a
vilkoz <vilko.a@gmail.com>
2017-08-12 04:11:26 +0300

    heroku config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,20 @@
 <?php
-$DB_DSN = 'mysql:dbname=tasker;host=localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
+if ($_SERVER['SERVER_NAME'] == 'camagru-vilkoz.herokuapp.com' ||
+	(isset($argv) && $argv[1] == 'heroku'))
+{
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url['host'];
+	$user = $url['user'];
+	$pass = $url['pass'];
+	$db = substr($url['path'], 1);
+	$DB_DSN = 'mysql:dbname=' . $db .';host=' . $host;
+	$DB_USER = $user;
+	$DB_PASS = $pass;
+}
+else
+{
+	$DB_DSN = 'mysql:dbname=tasker;host=localhost';
+	$DB_USER = 'root';
+	$DB_PASS = '';
+}
 ?>
